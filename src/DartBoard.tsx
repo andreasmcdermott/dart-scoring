a31f39cab4c9a5d871b1a951ca00ee96c7a676e2
Andreas McDermott <andreasmcdermott@gmail.com>
2025-05-27 17:50:36 -0700

    Cleanup a bit.

--- src/DartBoard.tsx
+++ src/DartBoard.tsx
@@ -2,7 +2,7 @@ import { For } from 'solid-js';
 
 const DART_BOARD_NUMBERS = [20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5];
 
-const DartBoard = () => {
+const DartBoard = ({onScore}: {onScore: (score: number, multiplier: number) => void}) => {
   const centerX = 200;
   const centerY = 200;
   const outerRadius = 180;
@@ -32,10 +32,6 @@ const DartBoard = () => {
     return `M ${x1} ${y1} L ${x2} ${y2} A ${outerRadius} ${outerRadius} 0 ${largeArcFlag} 1 ${x3} ${y3} L ${x4} ${y4} A ${innerRadius} ${innerRadius} 0 ${largeArcFlag} 0 ${x1} ${y1}`;
   };
 
-  const handleSegmentClick = (number: number, multiplier: number) => {
-    console.log(`Clicked: ${multiplier}x${number} = ${multiplier * number}`);
-  };
-
   return (
     <div class="flex justify-center items-center min-h-screen bg-gray-100">
       <svg width="400" height="400" viewBox="0 0 400 400">
@@ -56,7 +52,7 @@ const DartBoard = () => {
                   stroke="#fff"
                   stroke-width="0"
                   class="cursor-pointer hover:opacity-80"
-                  onClick={() => handleSegmentClick(number, 1)}
+                  onClick={() => onScore(number, 1)}
                 />
                 
                 {/* Double segment */}
@@ -66,7 +62,7 @@ const DartBoard = () => {
                   stroke="#fff"
                   stroke-width="1"
                   class="cursor-pointer hover:opacity-80"
-                  onClick={() => handleSegmentClick(number, 2)}
+                  onClick={() => onScore(number, 2)}
                 />
                 
                 {/* Inner single segment */}
@@ -76,7 +72,7 @@ const DartBoard = () => {
                   stroke="#fff"
                   stroke-width="1"
                   class="cursor-pointer hover:opacity-80"
-                  onClick={() => handleSegmentClick(number, 1)}
+                  onClick={() => onScore(number, 1)}
                 />
                 
                 {/* Treble segment */}
@@ -86,7 +82,7 @@ const DartBoard = () => {
                   stroke="#fff"
                   stroke-width="1"
                   class="cursor-pointer hover:opacity-80"
-                  onClick={() => handleSegmentClick(number, 3)}
+                  onClick={() => onScore(number, 3)}
                 />
                 
                 {/* Inner single segment */}
@@ -96,7 +92,7 @@ const DartBoard = () => {
                   stroke="#fff"
                   stroke-width="1"
                   class="cursor-pointer hover:opacity-80"
-                  onClick={() => handleSegmentClick(number, 1)}
+                  onClick={() => onScore(number, 1)}
                 />
                 
                 {/* Number text */}
@@ -126,7 +122,7 @@ const DartBoard = () => {
           stroke="#fff"
           stroke-width="2"
           class="cursor-pointer hover:opacity-80"
-          onClick={() => handleSegmentClick(25, 1)}
+          onClick={() => onScore(25, 1)}
         />
         
         {/* Inner bull/bullseye (50 points) */}
@@ -138,7 +134,7 @@ const DartBoard = () => {
           stroke="#fff"
           stroke-width="2"
           class="cursor-pointer hover:opacity-80"
-          onClick={() => handleSegmentClick(50, 1)}
+          onClick={() => onScore(50, 1)}
         />
       </svg>
     </div>
